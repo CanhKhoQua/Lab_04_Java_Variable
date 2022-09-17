55fcf36b216c5084a81be9799303291a8263fbd7
LocNguyen <nguye2lc@mail.uc.edu>
2022-09-17 14:48:54 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int intOperandA = 12;
+        int intOperandB =16;
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        int intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The Modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        double doubleOperandA = 12;
+        double doubleOperandB =16;
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        double doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The Modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+
+
+        int kids = 3;
+        System.out.println(kids);
+
+        boolean isRaining = false;
+        System.out.println(isRaining);
+
+        double price = 3.78;
+        System.out.println(price);
+
+        int favNum = 8;
+        System.out.println(favNum);
+
+        double size = 9.0;
+        System.out.println(size);
+
+        int birthMonth = 8;
+        System.out.println(birthMonth);
+
+        String fullName = "Loc Nguyen";
+        System.out.println(fullName);
+
+    }
+}
